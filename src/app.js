f1ca896d0d59239d9c9c2e8b2d89aa3d41617c82
jasonbarry96 <jasonbarry96@gmail.com>
2021-07-19 12:47:45 -0400

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,57 @@
+/* global algoliasearch instantsearch */
+
+const searchClient = algoliasearch(
+  'latency',
+  '6be0576ff61c053d5f9a3225e2a90f76'
+);
+
+const search = instantsearch({
+  indexName: 'instant_search',
+  searchClient,
+});
+
+let previousQuery = '';
+
+search.addWidget(
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+    searchAsYouType: false,
+    queryHook(query, f) {
+      if (previousQuery !== query) {
+        search.helper.clearRefinements();
+      }
+
+      previousQuery = query;
+      f(query);
+    },
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.hits({
+    container: '#hits',
+    templates: {
+      item: `
+        <article>
+          <h1>{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}</h1>
+          <p>{{#helpers.highlight}}{ "attribute": "description" }{{/helpers.highlight}}</p>
+        </article>
+      `,
+    },
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.refinementList({
+    container: '#brand-list',
+    attribute: 'brand',
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.pagination({
+    container: '#pagination',
+  })
+);
+
+search.start();
